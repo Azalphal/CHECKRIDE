fb6aee589518956b90e15deec743c6ba60d1ed5a
Azalphal <mael.nouvel@componize.com>
2024-02-23 09:26:40 +0100

    Initial Commit

---
+++ src/main/java/com/gearshifter/checkride/CheckrideApplication.java
@@ -0,0 +1,13 @@
+package com.gearshifter.checkride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckrideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckrideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gearshifter/checkride/CheckrideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gearshifter.checkride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckrideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
